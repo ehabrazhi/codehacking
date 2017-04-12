9c8ebd3f239b03962fb2e6d3389bb37e26599ad8
Ehab <e.razhi@gmail.com>
2017-04-12 20:37:10 +0300

    admin master page setup

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,8 +1,20 @@
-<?php
+@extends('layouts.admin')
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+
+
+
+
+@section('content')
+
+
+    <h1>Admin</h1>
+
+
+
+@stop
+
+@section('footer')
+
+
+@stop
